e900d3723b75b41e30255af1135f4172847aee8a
alferdousrana13 <alferdousrana@gmail.com>
2026-05-05 17:41:07 +0600

    Install Firebase

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>Money Dashboard</h1>
+      <p>Track your income, expenses, and monthly balance.</p>
+    </main>
+  );
+}
+
+export default App;
